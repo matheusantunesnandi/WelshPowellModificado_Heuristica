0cb080b3de705d594f063dc620a32c247411de53
Matheus A. Nandi <matheusan@users.noreply.github.com>
2018-06-15 12:39:26 -0300

    Ajustes insignificantes

--- grafos/src/Principal.java
+++ grafos/src/Principal.java
@@ -7,8 +7,8 @@ public class Principal {
 		// executarTrabalho3();
 		// executarAlgoritmoFordFulkerson();
 
-		// executarSeminario();
-		executarAlgoritmoWelshPowell();
+//		executarSeminario();
+		 executarAlgoritmoWelshPowell();
 	}
 
 	public static void executarSeminario() {
@@ -34,9 +34,10 @@ public class Principal {
 		g.adicionarAresta("c", "t", 10.0);
 
 		AlgoritmoColoracaoHeuristica ach = new AlgoritmoColoracaoHeuristica(g);
+		// System.out.println("Exemplo do professor:");
 		// ach.imprimirNumeroCromatico();
 
-		// Bibartido
+		// Bipartido
 		g = new Grafo();
 
 		g.adicionarVertice("s");
@@ -57,9 +58,10 @@ public class Principal {
 		g.adicionarAresta("c", "a", 10.0);
 
 		ach = new AlgoritmoColoracaoHeuristica(g);
+		System.out.println("Bipartido");
 		ach.imprimirNumeroCromatico();
 
-		// Exemplo do vídeo
+		// Exemplo do vídeo:
 		g = new Grafo();
 		g.adicionarVertice("a");
 		g.adicionarVertice("b");
@@ -98,8 +100,8 @@ public class Principal {
 		g.adicionarAresta("j", "l", 0.0);
 
 		ach = new AlgoritmoColoracaoHeuristica(g);
+		System.out.println("Exemplo do vídeo:");
 		ach.imprimirNumeroCromatico();
-
 	}
 
 	public static void executarAlgoritmoFordFulkerson() {
@@ -129,11 +131,8 @@ public class Principal {
 	}
 
 	public static void executarAlgoritmoWelshPowell() {
-
 		// Exemplo do professor:
 		Grafo g = new Grafo();
-
-		// g.setOrientado(true);
 
 		g.adicionarVertice("s");
 		g.adicionarVertice("a");
@@ -153,22 +152,73 @@ public class Principal {
 		g.adicionarAresta("c", "t", 10.0);
 
 		AlgoritmoWelshPowell awp = new AlgoritmoWelshPowell(g);
-		awp.imprimirNumeroCromatico();
+		// System.out.println("Exemplo do professor:");
+		// awp.imprimirNumeroCromatico();
 
-		// Bibartido
+		// Bipartido
 		g = new Grafo();
 
 		g.adicionarVertice("s");
 		g.adicionarVertice("a");
 		g.adicionarVertice("b");
 		g.adicionarVertice("c");
+		g.adicionarVertice("d");
+		g.adicionarVertice("t");
 
 		g.adicionarAresta("s", "b", 10.0);
 		g.adicionarAresta("s", "c", 10.0);
 		g.adicionarAresta("a", "b", 10.0);
 		g.adicionarAresta("a", "c", 10.0);
 
+		g.adicionarAresta("b", "s", 10.0);
+		g.adicionarAresta("b", "a", 10.0);
+		g.adicionarAresta("c", "s", 10.0);
+		g.adicionarAresta("c", "a", 10.0);
+
 		awp = new AlgoritmoWelshPowell(g);
+		System.out.println("Bipartido");
+		awp.imprimirNumeroCromatico();
+
+		// Exemplo do vídeo:
+		g = new Grafo();
+		g.adicionarVertice("a");
+		g.adicionarVertice("b");
+		g.adicionarVertice("c");
+		g.adicionarVertice("d");
+		g.adicionarVertice("e");
+		g.adicionarVertice("f");
+		g.adicionarVertice("g");
+		g.adicionarVertice("h");
+		g.adicionarVertice("i");
+		g.adicionarVertice("j");
+		g.adicionarVertice("k");
+		g.adicionarVertice("l");
+
+		g.adicionarAresta("a", "b", 0.0);
+		g.adicionarAresta("a", "f", 0.0);
+		g.adicionarAresta("a", "g", 0.0);
+		g.adicionarAresta("b", "c", 0.0);
+		g.adicionarAresta("b", "g", 0.0);
+		g.adicionarAresta("b", "h", 0.0);
+		g.adicionarAresta("c", "d", 0.0);
+		g.adicionarAresta("c", "g", 0.0);
+		g.adicionarAresta("c", "j", 0.0);
+		g.adicionarAresta("d", "e", 0.0);
+		g.adicionarAresta("d", "g", 0.0);
+		g.adicionarAresta("e", "f", 0.0);
+		g.adicionarAresta("e", "g", 0.0);
+		g.adicionarAresta("f", "g", 0.0);
+		g.adicionarAresta("h", "i", 0.0);
+		g.adicionarAresta("h", "k", 0.0);
+		g.adicionarAresta("h", "l", 0.0);
+		g.adicionarAresta("i", "j", 0.0);
+		g.adicionarAresta("i", "k", 0.0);
+		g.adicionarAresta("i", "l", 0.0);
+		g.adicionarAresta("j", "k", 0.0);
+		g.adicionarAresta("j", "l", 0.0);
+
+		awp = new AlgoritmoWelshPowell(g);
+		System.out.println("Exemplo do vídeo:");
 		awp.imprimirNumeroCromatico();
 	}
 
