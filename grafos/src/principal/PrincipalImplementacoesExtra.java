034adb75e19e64388e3e72505231ea93573076da
Gabriel Cavalcante <gabrielcavalcante1021@gmail.com>
2018-06-18 14:40:16 -0300

    Terminando de adicionar tipo de materiáis explosivos
    Provavelmente o último commit.

--- grafos/src/principal/PrincipalImplementacoesExtra.java
+++ grafos/src/principal/PrincipalImplementacoesExtra.java
@@ -1,6 +1,5 @@
 package principal;
 
-import algoritmo.AlgoritmoFordFulkerson;
 import algoritmo.AlgoritmoWelshPowell;
 import grafo.Grafo;
 //classe principal
@@ -13,31 +12,31 @@ public class PrincipalImplementacoesExtra {
 
 	}
 	//não é mais relevantes para o trabalho
-	public static void executarAlgoritmoFordFulkerson() {
-		Grafo g = new Grafo();
-
-		g.setOrientado(true);
-
-		g.adicionarVertice("Pólvora");
-		g.adicionarVertice("a");
-		g.adicionarVertice("Nitrato de Amônio");
-		g.adicionarVertice("C4");
-		g.adicionarVertice("d");
-		g.adicionarVertice("t");
-
-		g.adicionarAresta("Pólvora", "a", 10.0);
-		g.adicionarAresta("Pólvora", "b", 10.0);
-		g.adicionarAresta("a", "c", 4.0);
-		g.adicionarAresta("a", "d", 8.0);
-		g.adicionarAresta("a", "b", 2.0);
-		g.adicionarAresta("b", "d", 9.0);
-		g.adicionarAresta("d", "c", 6.0);
-		g.adicionarAresta("d", "t", 10.0);
-		g.adicionarAresta("c", "t", 10.0);
-
-		AlgoritmoFordFulkerson aff = new AlgoritmoFordFulkerson(g, "Pólvora", "t");
-		aff.imprimirValorFluxoMaximo();
-	}
+//	public static void executarAlgoritmoFordFulkerson() {
+//		Grafo g = new Grafo();
+//
+//		g.setOrientado(true);
+//
+//		g.adicionarVertice("Pólvora");
+//		g.adicionarVertice("a");
+//		g.adicionarVertice("Nitrato de Amônio");
+//		g.adicionarVertice("C4");
+//		g.adicionarVertice("d");
+//		g.adicionarVertice("Trinitrotolueno");
+//
+//		g.adicionarAresta("Pólvora", "a", 10.0);
+//		g.adicionarAresta("Pólvora", "b", 10.0);
+//		g.adicionarAresta("a", "c", 4.0);
+//		g.adicionarAresta("a", "d", 8.0);
+//		g.adicionarAresta("a", "b", 2.0);
+//		g.adicionarAresta("b", "d", 9.0);
+//		g.adicionarAresta("d", "c", 6.0);
+//		g.adicionarAresta("d", "Trinitrotolueno", 10.0);
+//		g.adicionarAresta("c", "Trinitrotolueno", 10.0);
+//
+//		AlgoritmoFordFulkerson aff = new AlgoritmoFordFulkerson(g, "Pólvora", "Trinitrotolueno");
+//		aff.imprimirValorFluxoMaximo();
+//	}
 
 	public static void executarAlgoritmoWelshPowell() {
 		// Exemplo do professor:
@@ -47,18 +46,18 @@ public class PrincipalImplementacoesExtra {
 		g.adicionarVertice("Nitroglicerina");
 		g.adicionarVertice("Nitrato de Amônio");
 		g.adicionarVertice("C4");
-		g.adicionarVertice("d");
-		g.adicionarVertice("t");
+		g.adicionarVertice("C3");
+		g.adicionarVertice("Trinitrotolueno");
 
 		g.adicionarAresta("Pólvora", "Nitroglicerina", 10.0);
 		g.adicionarAresta("Pólvora", "Nitrato de Amônio", 10.0);
 		g.adicionarAresta("Nitroglicerina", "C4", 4.0);
-		g.adicionarAresta("Nitroglicerina", "d", 8.0);
+		g.adicionarAresta("Nitroglicerina", "C3", 8.0);
 		g.adicionarAresta("Nitroglicerina", "Nitrato de Amônio", 2.0);
-		g.adicionarAresta("Nitrato de Amônio", "d", 9.0);
-		g.adicionarAresta("d", "C4", 6.0);
-		g.adicionarAresta("d", "t", 10.0);
-		g.adicionarAresta("C4", "t", 10.0);
+		g.adicionarAresta("Nitrato de Amônio", "C3", 9.0);
+		g.adicionarAresta("C3", "C4", 6.0);
+		g.adicionarAresta("C3", "Trinitrotolueno", 10.0);
+		g.adicionarAresta("C4", "Trinitrotolueno", 10.0);
 
 		AlgoritmoWelshPowell awp = new AlgoritmoWelshPowell(g);
 		// System.out.println("Exemplo do professor:");
@@ -71,8 +70,8 @@ public class PrincipalImplementacoesExtra {
 		g.adicionarVertice("Nitroglicerina");
 		g.adicionarVertice("Nitrato de Amônio");
 		g.adicionarVertice("C4");
-		g.adicionarVertice("d");
-		g.adicionarVertice("t");
+		g.adicionarVertice("C3");
+		g.adicionarVertice("Trinitrotolueno");
 
 		g.adicionarAresta("Pólvora", "Nitrato de Amônio", 10.0);
 		g.adicionarAresta("Pólvora", "C4", 10.0);
@@ -93,7 +92,7 @@ public class PrincipalImplementacoesExtra {
 		g.adicionarVertice("Nitroglicerina");
 		g.adicionarVertice("Nitrato de Amônio");
 		g.adicionarVertice("C4");
-		g.adicionarVertice("d");
+		g.adicionarVertice("C3");
 		g.adicionarVertice("e");
 		g.adicionarVertice("f");
 		g.adicionarVertice("g");
@@ -109,11 +108,11 @@ public class PrincipalImplementacoesExtra {
 		g.adicionarAresta("Nitrato de Amônio", "C4", 0.0);
 		g.adicionarAresta("Nitrato de Amônio", "g", 0.0);
 		g.adicionarAresta("Nitrato de Amônio", "h", 0.0);
-		g.adicionarAresta("C4", "d", 0.0);
+		g.adicionarAresta("C4", "C3", 0.0);
 		g.adicionarAresta("C4", "g", 0.0);
 		g.adicionarAresta("C4", "j", 0.0);
-		g.adicionarAresta("d", "e", 0.0);
-		g.adicionarAresta("d", "g", 0.0);
+		g.adicionarAresta("C3", "e", 0.0);
+		g.adicionarAresta("C3", "g", 0.0);
 		g.adicionarAresta("e", "f", 0.0);
 		g.adicionarAresta("e", "g", 0.0);
 		g.adicionarAresta("f", "g", 0.0);
